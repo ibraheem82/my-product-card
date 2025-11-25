e54c94ce7ddecd2912c7270a0fbcadd8039a6196
Ibraheem Omikunle <84424537+ibraheem82@users.noreply.github.com>
2025-11-25 12:56:10 +0100

    Create Price component to display product price

---
+++ src/components/Price.jsx
@@ -0,0 +1,9 @@
+// src/components/Price.jsx
+import React from "react";
+import product from "../product";
+
+const Price = () => {
+  return <h4 style={{ color: "green" }}>{product.price}</h4>;
+};
+
+export default Price;
